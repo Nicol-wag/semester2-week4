0973e727d62fc54df4687e5923b3ae3fb5c651be
Nicol-wag <nhbd0465@leeds.ac.uk>
2026-02-20 13:03:37 +0000

    workfinish2.10

--- session-2/task4/scan_sum.c
+++ session-2/task4/scan_sum.c
@@ -4,9 +4,19 @@
 int main( void ) {
 
     // define suitable data
-
+    int a,b;
     // use scanf to read from the terminal
     // print the output from scanf and the data values 
+    
+    printf("Please enter two integers: ");
+    int count = scanf("%d %d", &a, &b);
+
+
+    printf("read %d value\n", count);
+    printf("value: %d, %d\n", a, b);
+
+    printf("Sum = %d\n", a + b);
+
 
     return 0;
 }
